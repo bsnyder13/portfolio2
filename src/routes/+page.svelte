6a36b0c19bb026fdea4a11a7c8b7aa4ff16460c5
Brad Snyder <bsnyder13@gmail.com>
2024-01-25 22:19:55 -0500

    Played with fonts and colors a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <h1>Brad Snyder</h1>
 
-<p>I'm Brad Snyder, an aspiring web developer with 20 years of customer support experience and 13 years of technical support experience.</p>
+<p>I'm Brad Snyder, an aspiring web developer with 20 years of customer support experience and 13 years of technical support experience. </p>
 
+<style>
+    h1 {
+        color: #5800FF;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 5em;
+    }
 
+    p {
+        color: #000000;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 2em;
+    }
 
+</style>
+<!-- other colors in palette are:
+#E900FF
+#FFC600 -->
