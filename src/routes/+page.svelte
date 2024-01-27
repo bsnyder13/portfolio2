e7607ec47462001d6c7cac8399eab087aada767b
Brad Snyder <bsnyder13@gmail.com>
2024-01-26 23:04:05 -0500

    Played with css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,18 @@
         color: #5800FF;
         font-family: 'Courier New', Courier, monospace;
         font-size: 6em;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: left;
     }
 
     p {
         color: #000000;
         font-family: 'Courier New', Courier, monospace;
         font-size: 2em;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: left;
     }
 
 </style>
